48005bb74eb1bf8b04c66ebbce9aa20d9d8291c3
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-01-05 15:58:11 -0300

    add boilerplate for the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import Layout from './components/Layout';
+
+import Add from './pages/Add';
+import Expenses from './pages/Expenses';
+import Home from './pages/Home';
+import Incomes from './pages/Incomes';
+
 export default function App() {
   return (
-    <div>
-      App.js
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/expenses" element={<Expenses />} />
+        <Route path="/incomes" element={<Incomes />} />
+      </Routes>
+    </Layout>
   )
 }
