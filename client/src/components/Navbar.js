3c2a467129322ad17e187976c21a8c68c81a99da
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-01-04 23:33:20 -0300

    make navbar responsive

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,26 +13,24 @@ export default function Navbar() {
     return (
         <nav className={styles.nav}>
             <MaxWidthWrapper>
-                <div className={styles.wrapper}>
-                    <div className={styles.navItem}>
+                <ul className={styles.navItems}>
+                    <li className={styles.navItem}>
                         <FontAwesomeIcon icon={faHome} />
                         Inicio
-                    </div>
-                    <ul className={styles.navItems}>
-                        <li className={styles.navItem}>
-                            <FontAwesomeIcon icon={faPlus} />
-                            Agregar
-                        </li>
-                        <li className={styles.navItem}>
-                            <FontAwesomeIcon icon={faArrowCircleDown} />
-                            Ingresos
-                        </li>
-                        <li className={styles.navItem}>
-                            <FontAwesomeIcon icon={faArrowCircleUp} />
-                            Egresos
-                        </li>
-                    </ul>
-                </div>
+                    </li>
+                    <li className={styles.navItem}>
+                        <FontAwesomeIcon icon={faPlus} />
+                        Agregar
+                    </li>
+                    <li className={styles.navItem}>
+                        <FontAwesomeIcon icon={faArrowCircleDown} />
+                        Ingresos
+                    </li>
+                    <li className={styles.navItem}>
+                        <FontAwesomeIcon icon={faArrowCircleUp} />
+                        Egresos
+                    </li>
+                </ul>
             </MaxWidthWrapper>
         </nav>
     )
