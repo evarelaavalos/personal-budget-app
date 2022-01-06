41e885cbf85ef96f29ee1b77a42554c4ecf95f43
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-01-06 20:31:11 -0300

    add consistency to the naming

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -5,9 +5,9 @@ import styles from './TransactionList.module.css';
 export default function TransactionList() {
     return (
         <section className={styles.transactions}>
-            <div className={styles.transactionsHeader}>
-                <h2 className={styles.transactionsHeaderTitle}>Últimos Movimientos</h2>
-                <p className={styles.transactionsHeaderCaption}>Gestiona las últimas operaciones realizadas.</p>
+            <div className={styles.header}>
+                <h2 className={styles.title}>Últimos Movimientos</h2>
+                <p className={styles.caption}>Gestiona las últimas operaciones realizadas.</p>
             </div>
             <TransactionListRow
                 concept='Aca compraste algo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit provident aliquid doloribus quo mollitia praesentium architecto aut repellat rem harum.'
--- client/src/components/TransactionListRow.js
+++ client/src/components/TransactionListRow.js
@@ -11,33 +11,33 @@ import styles from './TransactionListRow.module.css';
 
 export default function TransactionListRow({ concept, amount, date, type }) {
     const typeElement = type === 'income' ? (
-        <div className={styles.transactionsRowType}>
+        <div className={styles.type}>
             <FontAwesomeIcon icon={faArrowCircleDown} size="3x" />
             Ingreso
         </div>
     ) : (
         /* type === 'expense' */
-        <div className={styles.transactionsRowType}>
+        <div className={styles.type}>
             <FontAwesomeIcon icon={faArrowCircleUp} size="3x" />
             Egreso
         </div>
     );
 
     return (
-        <div className={styles.transactionsRow}>
+        <div className={styles.transaction}>
             {typeElement}
-            <div className={styles.transactionsRowDescription}>
+            <div className={styles.concept}>
                 {concept}
             </div>
-            <div className={styles.transactionsRowMetadata}>
-                <div className={styles.transactionsRowMetadataAmount}>
+            <div className={styles.metadata}>
+                <div className={styles.metadataAmount}>
                     {amount}
                 </div>
-                <div className={styles.transactionsRowMetadataDate}>
+                <div className={styles.metadataDate}>
                     {date}
                 </div>
             </div>
-            <div className={styles.transactionsRowOptions}>
+            <div className={styles.options}>
                 <FontAwesomeIcon icon={faPen} />
                 <FontAwesomeIcon icon={faTrashAlt} />
             </div>
