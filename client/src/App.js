02006156ac33a43b573fcb139216df6a925a3b53
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-01-06 23:11:15 -0300

    move transactions to a state in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Routes, Route } from "react-router-dom";
 
 import Layout from './components/Layout';
@@ -8,13 +9,44 @@ import Home from './pages/Home';
 import Incomes from './pages/Incomes';
 
 export default function App() {
+  const [transactions, setTransactions] = useState([
+    {
+      id: 1,
+      concept: 'Aca compraste algo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit provident aliquid doloribus quo mollitia praesentium architecto aut repellat rem harum.',
+      amount: 215.23,
+      date: '20 de Diciembre',
+      type: 'income',
+    },
+    {
+      id: 2,
+      concept: 'Aca vendiste algo.',
+      amount: 215.23,
+      date: '20 de Diciembre',
+      type: 'income',
+    },
+    {
+      id: 3,
+      concept: 'Aca te depositaron algo.',
+      amount: 215.23,
+      date: '20 de Diciembre',
+      type: 'income',
+    },
+    {
+      id: 4,
+      concept: 'Aca compraste algo.',
+      amount: 215.23,
+      date: '20 de Diciembre',
+      type: 'expense',
+    },
+  ]);
+
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home transactions={transactions}/>} />
         <Route path="/add" element={<Add />} />
-        <Route path="/expenses" element={<Expenses />} />
-        <Route path="/incomes" element={<Incomes />} />
+        <Route path="/expenses" element={<Expenses transactions={transactions}/>} />
+        <Route path="/incomes" element={<Incomes transactions={transactions}/>} />
       </Routes>
     </Layout>
   )
--- client/src/pages/Expenses.js
+++ client/src/pages/Expenses.js
@@ -1,10 +1,13 @@
 import TransactionList from '../components/TransactionList';
 
-export default function Expenses() {
+export default function Expenses({ transactions }) {
+    const expenses = transactions.filter(transaction => transaction.type === 'expense');
+
     return (
         <TransactionList
             title='Egresos'
             caption='Aquí se muestran los últimos gastos registrados.'
+            transactions={expenses}
         />
     )
 }
