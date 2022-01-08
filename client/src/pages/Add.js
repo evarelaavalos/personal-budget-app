891e8c9e688770c5728e88466ee7ef034965ffcc
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-01-08 03:46:43 -0300

    add validation to the form

--- client/src/pages/Add.js
+++ client/src/pages/Add.js
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import Button from '../components/Button';
 
 import styles from './Add.module.css';
@@ -11,8 +13,38 @@ export default function Add({ types }) {
 
     const today = new Date().toISOString().split("T")[0];
 
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        const concept = data.get('concept');
+        const date = new Date(data.get('date'));
+        const amount = Number(data.get('amount'));
+        const type = data.get('type');
+
+        const response = {
+            concept,
+            date,
+            amount,
+            type
+        };
+
+        console.log(response);
+
+        // TODO: Set success based on response.
+        const success = true;
+        if (success) {
+            // Do something
+            navigate('/');
+            
+        } else {
+            // Do something else
+        }
+    }
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
             <h3 className={styles.formTitle}>Agregar Transacción</h3>
             <label htmlFor="concept" className={styles.inputTitle}>
                 Concepto:
