7a51c7f3f2ed5dbb345ba3a90ca3decc59e95ca0
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-01-11 21:49:06 -0300

    change api dates format to YYYY-MM-DD

--- server/src/database/mysql.js
+++ server/src/database/mysql.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const moment = require('moment');
 
 const pool = mysql.createPool({
     host: 'localhost',
@@ -6,6 +7,13 @@ const pool = mysql.createPool({
     password: 'COMPLETE_WITH_YOUR_PASSWORD',
     port: 'ADD_YOUR_PORT_NUMBER',
     database: 'personal_budget_db',
+    typeCast: function (field, next) {
+        if (field.type === 'DATE') {
+            return moment(field.string()).format('YYYY-MM-DD');
+        } else {
+            return next();
+        }
+    },
 });
 
 async function getConnection() {
